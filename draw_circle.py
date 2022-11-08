f7b7d907cc182e10bae0306522464876930f7a19
whizygeek <pratik.hingu123@gmail.com>
2022-11-08 18:47:46 +0100

    first commit

---
+++ draw_circle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+from ast import While
+import rospy
+from geometry_msgs.msg import Twist
+
+if __name__ == '__main__':
+    rospy.init_node("draw circle")
+    rospy.loginfo("Node has been started.")
+
+    pub = rospy.Publisher("/turtle1/cmd_vel",Twist, queue_size=10)
+
+    rate = rospy.Rate(2)
+
+    while not rospy.is_shutdown():
+        #publish cmd vel
+        msg = Twist()
+        msg.linear.x=2.0
+        msg.angular.z=1.0
+        pub.publish(msg)
+        rate.sleep()
